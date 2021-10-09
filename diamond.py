efafd7b0ab835ec0e24653fafb949f26554a10ec
PCutMLN <abiyyudhawydzakwan751@gmail.com>
2021-10-09 12:11:37 +0700

    diamond tugas

--- diamond.py
+++ diamond.py
@@ -4,27 +4,27 @@ def diamond1(n):
         j = 0
         k = 0
         while j <= n-i:
-            print(" ", end=" ")
+            print(" ",end=" ")
             j = j + 1
         while k <= 2*i:
-            print("*", end=" ")
+            print("*",end=" ")
             k = k + 1
         print(" ")
         i = i + 1
 
 def diamond2(n):
-    i = 0
-    while i <= n:
+    i = n-1
+    while i >= 0:
         j = 0
         k = 0
-        while k <= n-i:
-            print("*", end=" ")
-            k = k + 1
-        while j <= i:
-            print(" ", end=" ")
+        while j <= n-i:
+            print(" ",end=" ")
             j = j + 1
+        while k <= 2*i:
+            print("*",end=" ")
+            k = k + 1
         print(" ")
-        i = i + 1
+        i = i - 1
 
 diamond1(3)
 diamond2(3)
